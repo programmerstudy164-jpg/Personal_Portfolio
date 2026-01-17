3dc5846db98e5d93468bc271b2a9320727d940a1
Marco <Kulikot2k15@gmail.com>
2026-01-17 09:47:13 +0800

    Home Pages

--- src/sections/Home.jsx
+++ src/sections/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
-
-const Home = ({ Dark }) => {
+import { MdVerified } from "react-icons/md";
+import { SlLocationPin } from "react-icons/sl";
+const Home = ({ Dark, TextColor }) => {
   return (
     <div
       className={`h-screen flex justify-center ${
@@ -8,14 +9,24 @@ const Home = ({ Dark }) => {
       }`}
     >
       <div className="max-w-4xl w-full flex justify-center items-center mx-10 mb-10 mt-5">
-        <div className=" flex flex-col  justify-center items-center">
-          <h1 className="text-5xl text-text-primary font-bold mb-4 animate-fadeIn">
-            Hi, I'M Rhine Marco Quibra
-          </h1>
-          <p className="text-2xl text-gray-300 mb-3">
+        <div className=" flex flex-col  justify-center items-center gap-5">
+          <div className="flex gap-3 items-center">
+            <h1 className="text-5xl text-text-primary font-bold animate-fadeIn">
+              Hi, I'M Rhine Marco Quibra
+            </h1>
+
+            <MdVerified className="w-5 h-5 text-text-primary" />
+          </div>
+          <div className="flex gap-2">
+            <SlLocationPin className={`${TextColor} w-8 h-8`} />
+            <h1 className={`text-2xl ${TextColor}`}>
+              Misamis Occidental, Philippines
+            </h1>
+          </div>
+          <p className={`text-2xl ${TextColor}`}>
             BSIT Student | Aspiring Full Stack Developer
           </p>
-          <p className="text-2xl text-gray-300 mb-8">From Front to Back</p>
+          <p className={`text-2xl ${TextColor}`}>From Front to Back</p>
         </div>
       </div>
     </div>
