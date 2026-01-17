0c8a808fc885a1c556e98d79e5db3b95fe827814
Marco <Kulikot2k15@gmail.com>
2026-01-17 18:52:11 +0800

    Navigation bar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,17 +3,16 @@ import React from "react";
 const navItems = [
   { name: "Home", id: "home" },
   { name: "About", id: "about" },
+  { name: "Project", id: "project" },
   { name: "Skills", id: "Skills" },
   { name: "Contact", id: "contact" },
 ];
-const Nav = ({ Dark }) => {
+const Nav = ({ TextColor }) => {
   return (
     <ul className="flex gap-8">
       {navItems.map((item) => (
         <li
-          className={`font-semibold text-md cursor-pointer ${
-            Dark ? "text-white" : "text-black"
-          }`}
+          className={`font-semibold text-md cursor-pointer hover:text-text-primary ${TextColor}`}
           key={item.id}
         >
           {item.name}
