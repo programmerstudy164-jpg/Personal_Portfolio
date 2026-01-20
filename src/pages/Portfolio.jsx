4a618425b4b63cb4ff1c251215e8201a4c41faeb
Marco <Kulikot2k15@gmail.com>
2026-01-20 16:56:47 +0800

    Portfolio

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -2,6 +2,10 @@ import React, { useState, useEffect } from "react";
 import Home from "../sections/Home";
 import About from "../sections/About";
 import Header from "../sections/Header";
+import Project from "../sections/Project";
+import Skills from "../sections/Skills";
+import Footer from "../sections/Footer";
+import Contact from "../sections/Contact";
 
 const Portfolio = () => {
   const [isDark, setIsDark] = useState(() => localStorage.theme === "dark");
@@ -15,11 +19,17 @@ const Portfolio = () => {
       localStorage.theme = "light";
     }
   }, [isDark]);
+  const Text = isDark ? "text-gray-300" : "text-black";
+  const bgColor = isDark ? "bg-bg-primary" : "bg-white";
   return (
     <div>
-      <Header Dark={isDark} setDark={setIsDark} />
-      <Home Dark={isDark} />
-      <About />
+      <Header Dark={isDark} setDark={setIsDark} TextColor={Text} />
+      <Home TextColor={Text} bg={bgColor} />
+      <About TextColor={Text} bg={bgColor} />
+      <Project TextColor={Text} bg={bgColor} dark={isDark} />
+      <Skills TextColor={Text} bg={bgColor} dark={isDark} />
+      <Contact TextColor={Text} bg={bgColor} dark={isDark} />
+      <Footer TextColor={Text} bg={bgColor} dark={isDark} />
     </div>
   );
 };
