a309cac93d39c834fabb1935a784e69b7fb70c22
Marco <Kulikot2k15@gmail.com>
2026-01-31 10:45:29 +0800

    update Home layout

--- src/sections/Home.jsx
+++ src/sections/Home.jsx
@@ -2,24 +2,34 @@ import React from "react";
 import { MdVerified } from "react-icons/md";
 import { SlLocationPin } from "react-icons/sl";
 const Home = ({ TextColor, bg }) => {
-  return (
-    <div id="home" className={`h-screen flex justify-center ${bg}`}>
-      <div className="max-w-4xl w-full flex justify-center items-center mx-10">
-        <div className=" flex flex-col  justify-center items-center gap-5">
-          <div className="flex gap-3  items-center">
-            <h1 className="text-5xl text-text-primary font-bold animate-fadeIn">
-              Hi, I'M Rhine Marco Quibra
-            </h1>
 
-            <MdVerified className="w-5 h-5 mt-2 text-text-primary" />
-          </div>
-          <div className="flex gap-2">
+  return (
+    <div id="home" className={`min-h-screen flex justify-center ${bg}`}>
+      <div className="max-w-4xl w-full flex justify-center items-center">
+        <div className=" flex flex-col items-center gap-5">
+        
+  <h1 className="text-3xl sm:text-4xl md:text-5xl text-center text-text-primary font-bold animate-fadeIn flex flex-wrap justify-center items-center gap-2">
+
+    <span>Hi,</span>
+    <span>I'M Rhine Marco</span>
+
+   
+    <span className="flex items-center gap-1 whitespace-nowrap">
+      Quibra
+      <MdVerified className="w-5 h-5 sm:w-6 sm:h-6 text-text-primary" />
+    </span>
+
+  </h1>
+
+
+          <div className="flex gap-2 flex-wrap justify-center items-center">
             <SlLocationPin className={`${TextColor} w-8 h-8`} />
-            <h1 className={`text-2xl ${TextColor}`}>
+            <h1 className={`sm:text-xl md:text-2xl text-center ${TextColor}`}>
               Misamis Occidental, Philippines
             </h1>
           </div>
-          <p className={`text-2xl ${TextColor}`}>
+
+          <p className={`text-2xl ${TextColor} text-center`}>
             BSIT Student | Aspiring Full Stack Developer
           </p>
           <p className={`text-2xl ${TextColor}`}>From Front to Back</p>
