a15199597cd0071975b8b16d8c9c2c1214fcc7fe
Marco <Kulikot2k15@gmail.com>
2026-01-31 11:51:09 +0800

    update About layout

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -2,12 +2,12 @@ import React from "react";
 
 const About = ({ TextColor, bg }) => {
   return (
-    <div id="about" className={`h-screen flex justify-center ${bg}`}>
-      <div className="w-full max-w-4xl flex flex-col  items-center gap-15 pt-40">
+    <div id="about" className={`min-h-screen flex justify-center ${bg}`}>
+      <div className="w-full max-w-4xl flex flex-col  items-center gap-15 pt-32 px-3">
         <h1 className={`font-bold text-4xl text-center ${TextColor}`}>
           About Me
         </h1>
-        <div className={`${TextColor} max-w-2xl text-2xl text-center`}>
+        <div className={`${TextColor} max-w-2xl md:text-2xl text-xl text-center`}>
           Hi! I'm Rhine Marco Quibra, a BSIT student passionate about web
           development and building user-friendly applications. I enjoy creating
           full-stack web projects and learning new technologies.
